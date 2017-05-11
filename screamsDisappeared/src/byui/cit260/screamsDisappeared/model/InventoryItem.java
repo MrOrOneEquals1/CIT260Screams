53ed6dfe9a23cbf57ad55139467be74f2388e222
lauravaleriogibbs <lauravaleriogibbs@192.168.0.8>
2017-05-11 16:56:55 -0400

    5/11/2017- Inventory Item Class

---
+++ screamsDisappeared/src/byui/cit260/screamsDisappeared/model/InventoryItem.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.screamsDisappeared.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author lauravaleriogibbs
+ */
+public class InventoryItem implements Serializable {
+ private String inventoryType;
+ private double quantityInStock;
+ private double requiredAmount;
+
+    public InventoryItem() {
+    }
+ 
+ 
+
+    public String getInventoryType() {
+        return inventoryType;
+    }
+
+    public void setInventoryType(String inventoryType) {
+        this.inventoryType = inventoryType;
+    }
+
+    public double getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(double quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public double getRequiredAmount() {
+        return requiredAmount;
+    }
+
+    public void setRequiredAmount(double requiredAmount) {
+        this.requiredAmount = requiredAmount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.inventoryType);
+        hash = 89 * hash + (int) (Double.doubleToLongBits(this.quantityInStock) ^ (Double.doubleToLongBits(this.quantityInStock) >>> 32));
+        hash = 89 * hash + (int) (Double.doubleToLongBits(this.requiredAmount) ^ (Double.doubleToLongBits(this.requiredAmount) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem{" + "inventoryType=" + inventoryType + ", quantityInStock=" + quantityInStock + ", requiredAmount=" + requiredAmount + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItem other = (InventoryItem) obj;
+        return true;
+    }
+
+
+}
