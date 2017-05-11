9dc95bbf5ca05747e7641e0ef2639878b35e9b37
lauravaleriogibbs <lauravaleriogibbs@192.168.0.8>
2017-05-11 16:59:05 -0400

    5/11/2017- Inventory Item Class

--- screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
+++ screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
@@ -6,6 +6,7 @@
 package screamsdisappeared;
 
 import byui.cit260.screamsDisappeared.model.Actor;
+import byui.cit260.screamsDisappeared.model.InventoryItem;
 import byui.cit260.screamsDisappeared.model.Player;
 
 /**
@@ -35,8 +36,31 @@ public class ScreamsDisappeared {
         String actorInfo = actorOne.toString();
         System.out.println(actorInfo);
         
-           
+     InventoryItem wallet   = new InventoryItem();    
+     InventoryItem weapons  = new InventoryItem();    
+     InventoryItem backpack = new InventoryItem(); 
+    
+     wallet.setInventoryType ("Money");
+     wallet.setQuantityInStock(30);
+     wallet.setRequiredAmount (50);
+     
+     String inventoryItemInfo = wallet.toString();
+        System.out.println(inventoryItemInfo);
+     
+     weapons.setInventoryType ("Tools");
+     weapons.setQuantityInStock(50);
+     weapons.setRequiredAmount (20);
+     String inventoryItemInfo2 = weapons.toString();
+        System.out.println(inventoryItemInfo2);
+     
         
+     backpack.setInventoryType ("Resources");
+     backpack.setQuantityInStock(1);
+     backpack.setRequiredAmount (1);
+     
+     String inventoryItemInfo3 = backpack.toString();
+        System.out.println(inventoryItemInfo3);
+     
     }
     
 }
