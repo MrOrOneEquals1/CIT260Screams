98aaa9370d9367062be80e620d9bc0e62dd44fcb
Darin <Darin@WIN-2T9DQ39QA6F.localdomain>
2017-05-30 19:05:06 -0600

    Halfway through Program View

---
+++ screamsDisappeared/src/citbyui/cit260/screamsDisappeared/view/StartProgramView.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.screamsDisappeared.view;
+
+import byui.cit260.screamsDisappeared.model.Player;
+import java.util.Scanner;
+
+/**
+ *
+ * @author carriero
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    
+    public StartProgramView() {
+        
+        this.promptMessage = "\nPlease enter your name:";
+        //display the banner when view is created
+        this.displayBanner();
+    }
+
+    private void displayBanner() {
+        
+        System.out.println(
+                "\n**************************************************"
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                +""
+                
+        );
+    }
+
+    public void displayStartProgramView() {
+        
+        boolean done = false; // set flag to not done
+        do {
+            // prompt for and get players name
+            String playersName = this.getPlayersName();
+            if (playersName.toUpperCase().equals("Q")) // user want to quit
+                return; //exit the game
+            
+            // do the requested action and display the next view
+            done = this.doAction(playersName);
+            
+        } while (!done);
+    }
+
+    private String getPlayersName() {
+        Scanner keyboard = new Scanner(System.in);  //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; // initialize to not valid
+        
+        while(!valid) { // loop while an invalid value is enter
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+        
+        break;  // end the loop
+        
+        }
+    
+    return value; // return the value entered
+    
+    }
+
+    private boolean doAction(String playersName) {
+        
+        if (playersName.length() <2) {
+            System.out.println("\nInvalid Players Name:  The name must be greater than one character in length");
+            return false;
+        }
+        
+        //call createPlayer() control function
+        Player player = GameControl.createPlayer(playersName);
+        
+        if (player == null) { // if unsuccessful
+            System.out.println("\nError creating the player.");
+            return false;
+        }
+}
--- screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
+++ screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
@@ -5,18 +5,7 @@
  */
 package screamsdisappeared;
 
-import byui.cit260.screamsDisappeared.model.Actor;
-import byui.cit260.screamsDisappeared.model.Car;
-import byui.cit260.screamsDisappeared.model.EnemyScene;
-import byui.cit260.screamsDisappeared.model.Game;
-import byui.cit260.screamsDisappeared.model.InventoryItem;
-import byui.cit260.screamsDisappeared.model.Location;
-import byui.cit260.screamsDisappeared.model.Map;
-import byui.cit260.screamsDisappeared.model.Player;
-import byui.cit260.screamsDisappeared.model.Question;
-import byui.cit260.screamsDisappeared.model.QuestionScene;
-import byui.cit260.screamsDisappeared.model.RegularScenePlan;
-import byui.cit260.screamsDisappeared.model.ResourceScene;
+import citbyui.cit260.screamsDisappeared.view.StartProgramView;
 
 /**
  *
@@ -28,116 +17,9 @@ public class ScreamsDisappeared {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Player playerOne = new Player();
         
-            playerOne.setName("Hugo Vera");
-            playerOne.setBestTime(7.00);
-        
-            String playerInfo = playerOne.toString();
-            System.out.println(playerInfo);
-        
-        Game gameOne = new Game();
-        
-            gameOne.setNoPeople(2);
-            gameOne.setTotalTime(114);
-        
-            String gameInfo = gameOne.toString();
-            System.out.println(gameInfo);
-            
-        Map mapOne = new Map();
-        
-            mapOne.setDescription("This is a descripton of the map");
-            mapOne.setRowCount(5);
-            mapOne.setColumnCount(5);
-            mapOne.setCurrentRow(2);
-            mapOne.setCurrentColumn(3);
-                  
-            String mapInfo = mapOne.toString();
-            System.out.println(mapInfo);
-         
-        Location locationOne = new Location();
-        
-            locationOne.setRow(1);
-            locationOne.setColumn(1);
-            locationOne.setVisited(false);
-            locationOne.setAmountRemaining(24.0);
-                             
-            String locationInfo = locationOne.toString();
-            System.out.println(locationInfo);  
-            
-     Actor actorOne= new Actor();   
-        
-       actorOne.setName(" DLH");
-       actorOne.setDescription("Description about DLH"); 
-       actorOne.setCoordinates("east, west, south.."); 
-        
-        String actorInfo = actorOne.toString();
-        System.out.println(actorInfo);
-        
-     InventoryItem wallet   = new InventoryItem();    
-     InventoryItem weapons  = new InventoryItem();    
-     InventoryItem backpack = new InventoryItem(); 
-    
-     wallet.setInventoryType ("Money");
-     wallet.setQuantityInStock(30);
-     wallet.setRequiredAmount (50);
-     
-     String inventoryItemInfo = wallet.toString();
-        System.out.println(inventoryItemInfo);
-     
-     weapons.setInventoryType ("Tools");
-     weapons.setQuantityInStock(50);
-     weapons.setRequiredAmount (20);
-     String inventoryItemInfo2 = weapons.toString();
-        System.out.println(inventoryItemInfo2);
-     
-        
-     backpack.setInventoryType ("Resources");
-     backpack.setQuantityInStock(1);
-     backpack.setRequiredAmount (1);
-     
-     String inventoryItemInfo3 = backpack.toString();
-        System.out.println(inventoryItemInfo3);
-     
-        
-   Car carOne= new Car();     
-        
-    carOne.setDrivenMiles(500);
-    carOne.setTankSize(14);
-   String carInfo = carOne.toString();
-        System.out.println(carInfo); 
-        
-   RegularScenePlan regularScenePlan1 = new RegularScenePlan();
-   regularScenePlan1.setDescription (" Description of the scene");
-   regularScenePlan1.setBlocked (" Blocked");
-   regularScenePlan1.setSymbol (" Symbol");
-   String regularScenePlanInfo = regularScenePlan1.toString();
-        System.out.println(regularScenePlanInfo);
-        
-    ResourceScene sceneOne = new ResourceScene();
-    sceneOne.setAmount(4);
-    String sceneInfo  = sceneOne.toString();
-    System.out.println(sceneInfo);
-    
-    EnemyScene sceneEnemy = new EnemyScene();
-    sceneEnemy.setRoomSize(4);
-    sceneEnemy.setZombiesQuantity(2);
-    String enemyInfo = sceneEnemy.toString();
-    System.out.println(enemyInfo);
-    
-    QuestionScene questionList = new QuestionScene();
-    questionList.setBonus(400);
-    questionList.setNoToAnswer("You cannot get the amount needed");
-    String questionInfo = questionList.toString();
-    System.out.println(questionInfo);
-    
-    
-    
-    Question questionOne = new Question();
-    questionOne.setQuestion("Do you want to go left or right");
-    questionOne.setAnswer("Yes");
-    String questionWork = questionOne.toString();
-    System.out.println(questionWork);
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.displayStartProgramView();
     
     } 
 }
