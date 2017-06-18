e40c3de864e310ec8f6d695c2cb3b5c904392dda
carriero <carriero@192.168.139.128>
2017-06-17 22:30:27 -0600

    Description and DescriptionGame

---
+++ screamsDisappeared/src/citbyui/cit260/screamsDisappeared/view/DescriptionGame.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.screamsDisappeared.view;
+
+/**
+ *
+ * @author carriero
+ */
+public class DescriptionGame extends View{
+    
+    public DescriptionGame() {
+            
+    
+           super (
+                Description.getDescription()
+                + "\n"
+                + "\n*****Options******"
+                + "\n"
+                + "\nH - Go talk to the House"
+                + "\nC - Go to the City Center"
+                + "\n"
+            
+        );
+    }
+    
+    @Override
+    public boolean doAction(String choice) {
+
+        choice = choice.toUpperCase();
+
+        boolean rtnValue = true;
+
+        switch (choice) {
+            case "H": // Go to the House with the Lights
+                this.HomeLightOn(); 
+                break;
+            case "L": // Display Description of Game
+                this.CityCenter();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try Again");
+                rtnValue = false;
+                break;
+
+        }
+
+        return rtnValue;
+    }
+
+    private void HomeLightOn() {
+        System.out.println("*** HomeLightOn called ***");
+    }
+
+    private void CityCenter() {
+        System.out.println("*** CityCenter called ***");
+    }
+
+    
+}
+
