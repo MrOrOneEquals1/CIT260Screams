fc01e2e6cd0ed738fbd928ea49e074cf6d8e4cf3
carriero <carriero@192.168.139.128>
2017-06-20 19:34:44 -0600

    Lesson 10 start work

--- screamsDisappeared/src/byui/cit260/screamsDisappeared/model/Actor.java
+++ screamsDisappeared/src/byui/cit260/screamsDisappeared/model/Actor.java
@@ -12,65 +12,31 @@ import java.util.Objects;
  *
  * @author lauravaleriogibbs
  */
-public class Actor implements Serializable {
+public enum Actor implements Serializable {
     
-    private String name;
-    private String description;
-    private String coordinates;
+    private final String description;
+    private final Point coordinates;
     
-    
-
-    public String getName() {
-        return name;
+    public Actor() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+    Actor(String description) {
+        this.description = description;
+        coordinates = new Point(1,1);
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getCoordinates() {
         return coordinates;
     }
 
-    public void setCoordinates(String coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 67 * hash + Objects.hashCode(this.name);
-        hash = 67 * hash + Objects.hashCode(this.description);
-        hash = 67 * hash + Objects.hashCode(this.coordinates);
-        return hash;
-    }
 
     @Override
     public String toString() {
         return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        return true;
-    }
-    
 }
