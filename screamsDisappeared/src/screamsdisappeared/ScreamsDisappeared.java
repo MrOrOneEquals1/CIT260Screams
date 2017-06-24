05d18a14893ba428f49977d3cfb54435d69f5159
David Vera <David Vera@Personal>
2017-06-23 23:32:23 -0500

    personal assignment

--- screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
+++ screamsDisappeared/src/screamsdisappeared/ScreamsDisappeared.java
@@ -7,7 +7,11 @@ package screamsdisappeared;
 
 import byui.cit260.screamsDisappeared.model.Game;
 import byui.cit260.screamsDisappeared.model.Player;
+import byui.cit260.screamsDisappeared.model.ZombieDogs;
 import citbyui.cit260.screamsDisappeared.view.StartProgramView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
@@ -22,6 +26,28 @@ public class ScreamsDisappeared {
         
         StartProgramView startProgramView = new StartProgramView();
         startProgramView.display();
+        
+        ZombieDogs Z1 = new ZombieDogs("Pug", "Small", "10 Kilos");
+        ZombieDogs Z2 = new ZombieDogs("Beagle", "Medium", "15 Kilos");
+        ZombieDogs Z3 = new ZombieDogs("Rottweiler", "Big", "30 Kilos");
+        ZombieDogs Z4 = new ZombieDogs("Bulldog", "Big", "35 Kilos");
+        ZombieDogs Z5 = new ZombieDogs("Doberman", "Big", "40 Kilos");
+        
+        List<ZombieDogs> zombieDogs = new ArrayList<>();
+        
+        zombieDogs.add(Z1);
+        zombieDogs.add(Z2);
+        zombieDogs.add(Z3);
+        zombieDogs.add(Z4);
+        zombieDogs.add(Z5);
+        
+        Collections.sort(zombieDogs);
+        
+        System.out.println("Race     \tSize     \tWeight");
+        for (ZombieDogs element: zombieDogs){
+            
+            System.out.println(element);
+        }
     }
 
     private static Game currentGame = null;
