97d35505c42fd2d97bc6a3ff0b722ae08dc3936e
lauravaleriogibbs <lauravaleriogibbs@172.20.10.6>
2017-07-01 23:45:52 -0400

    5/11/2017- Explosives Needed View

--- screamsDisappeared/src/screamsdisappeared/control/MoneyEarned.java
+++ screamsDisappeared/src/screamsdisappeared/control/MoneyEarned.java
@@ -15,25 +15,25 @@ public class MoneyEarned {
         double moneyPerZombie = 1.0;
         double moneyPerZombieDog = 0.5;
 
-        if (zombiesKilled < 0) {  //zombies killed is negative
-            System.out.println("\nInvalid value: value cannot be negative");
-            return -1;
-        }
+       // if (zombiesKilled < 0) {  //zombies killed is negative
+         //   System.out.println("\nInvalid value: value cannot be negative");
+           // return -1;
+      //  }
 
-        if (zombiesKilled > 25) {  //zombies killed is more than 25
-            System.out.println("\nInvalid value: value cannot be more than 25");
-            return -1;
-        }
+       // if (zombiesKilled > 25) {  //zombies killed is more than 25
+        //    System.out.println("\nInvalid value: value cannot be more than 25");
+         //   return -1;
+        //}
 
-        if (zombieDogsKilled < 0) {  //zombieDogs killed is negative
-            System.out.println("\nInvalid value: value cannot be negative");
-            return -1;
-        }
+       // if (zombieDogsKilled < 0) {  //zombieDogs killed is negative
+        //    System.out.println("\nInvalid value: value cannot be negative");
+         //   return -1;
+       // }
 
-        if (zombieDogsKilled > 25) {  //zombieDogs killed is more than 25
-            System.out.println("\nInvalid value: value cannot be more than 25");
-            return -1;
-        }
+        //if (zombieDogsKilled > 25) {  //zombieDogs killed is more than 25
+        //    System.out.println("\nInvalid value: value cannot be more than 25");
+         //   return -1;
+       // }
 
             double moneyFromZombies = (zombiesKilled * moneyPerZombie);
             double moneyFromZombieDogs = (zombieDogsKilled * moneyPerZombieDog);
